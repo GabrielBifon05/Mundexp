ed366bedd8abc6ebdb63e3b5d55e026d670e06ec
GabrielBifon05 <gabriel.bsouza@sptech.school>
2023-06-13 12:48:03 -0300

    tudo finalizado

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -1,4 +1,4 @@
--- Active: 1677248782109@@127.0.0.1@3306@Aluno
+-- Active: 1685642326019@@127.0.0.1@3306@AlunoDisciplina
 -- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
 -- Você precisa executar os comandos no banco de dados para criar as tabelas,
 -- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
--- src/models/avisoModel.js
+++ src/models/avisoModel.js
@@ -4,11 +4,11 @@ function listar() {
     console.log("ACESSEI O AVISO  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()");
     var instrucao = `
         SELECT 
-            a.id AS idAviso,
+            a.idAviso AS idAviso,
             a.titulo,
             a.descricao,
             a.fk_usuario,
-            u.id AS idUsuario,
+            u.idUsuario AS idUsuario,
             u.nomeUsuario,
             u.emailUsuario,
             u.senhaUsuario
@@ -24,11 +24,11 @@ function pesquisarDescricao(texto) {
     console.log("ACESSEI O AVISO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function pesquisarDescricao()");
     var instrucao = `
         SELECT 
-            a.id AS idAviso,
+            a.idAviso AS idAviso,
             a.titulo,
             a.descricao,
             a.fk_usuario,
-            u.id AS idUsuario,
+            u.idUsuario AS idUsuario,
             u.nomeUsuario,
             u.emailUsuario,
             u.senhaUsuario
@@ -45,11 +45,11 @@ function listarPorUsuario(idUsuario) {
     console.log("ACESSEI O AVISO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listarPorUsuario()");
     var instrucao = `
         SELECT 
-            a.id AS idAviso,
+            a.idAviso AS idAviso,
             a.titulo,
             a.descricao,
             a.fk_usuario,
-            u.id AS idUsuario,
+            u.idUsuario AS idUsuario,
             u.nomeUsuario,
             u.emailUsuario,
             u.senhaUsuario
